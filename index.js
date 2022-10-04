e2bbbf49b879a913589599bfc25d76c5565abecd
steven rosa <scrosa13@outlook.com>
2022-10-04 19:41:35 -0400

    updating index

--- index.js
+++ index.js
@@ -1,80 +1,138 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const createTeam = require("./src/template.js");
-const Employee = require("./lib/Employee");
+const generateTeam = require("./src/template.js");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
 let staffArray = [];
 
-const managerQuestions = [
-  {
-    type: "input",
-    name: "name",
-    message: "What is the name of the team manager?",
-    validate: (nameInput) => {
-      if (nameInput) {
-        return true;
-      } else {
-        console.log("Please enter the name of the team manager.");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is the ID of the team manager?",
-    validate: (idInput) => {
-      if (isNaN(idInput)) {
-        return "Please enter a valid ID.";
-      }
-      return true;
-    },
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "What is the email of the team manager?",
-    validate: (emailInput) => {
-      if (emailInput) {
-        return true;
-      } else {
-        console.log("Please enter the email of the team manager.");
-        return false;
-      }
-    },
-  },
-  {
-    type: "input",
-    name: "officeNumber",
-    message: "What is the office number of the team manager?",
-    validate: (officeNumberInput) => {
-      if (isNaN(officeNumberInput)) {
-        return "Please enter a valid office number.";
-      }
-      return true;
-    },
-  },
-];
+const startQuestionPrompt = async () => {
+    const userResponse = await inquirer.prompt([
+      {
+        type: "input",
+        message: "What is your name?",
+        name: "name",
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return console.log("Please enter your name.");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "What is your ID number?",
+        name: "id",
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return console.log("Please enter a valid ID number.");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return console.log("Please enter a valid email address.");
+          }
+          return true;
+        },
+      },
+      {
+        type: "list",
+        message: "What is your new role on the team?",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        message: "What is your office phone number?",
+        name: "officeNumber",
+        when: ({ role }) => role == "Manager",
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return console.log("Please enter a valid office number.");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "What is your github username?",
+        name: "github",
+        when: ({ role }) => role == "Engineer",
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return console.log("Please enter a valid github username.");
+          }
+          return true;
+        },
+      },
+      {
+        type: "input",
+        message: "What is the name of your school?",
+        name: "school",
+        when: ({ role }) => role == "Intern",
+        validate: function (answer) {
+          if (answer.length < 1) {
+            return console.log("Please enter a valid school name.");
+          }
+          return true;
+        },
+      },
+    ]);
+    if (userResponse.role === "Manager") {
+      let newManager = new Manager(
+        userResponse.name,
+        userResponse.id,
+        userResponse.email,
+        userResponse.officeNumber
+      );
+      staffArray.push(newManager);
+    } else if (basicAnswers.role === "Engineer") {
+      let newEngineer = new Engineer(
+        userResponse.name,
+        userResponse.id,
+        userResponse.email,
+        userResponse.github
+      );
+      staffArray.push(newEngineer);
+    } else {
+      let newIntern = new Intern(
+        userResponse.name,
+        userResponse.id,
+        userResponse.email,
+        userResponse.school
+      );
+      staffArray.push(newIntern);
+    }
+  };
 
+  async function initQuestions() {
+    await startQuestionPrompt();
+  
+    const nextStep = await inquirer.prompt([
+      {
+        name: "newMember",
+        type: "list",
+        choices: ["Add new team member", "Create team"],
+        message: "What would you like to do next?",
+      },
+    ]);
+  
+    if (nextStep.newMember === "Add new team member") {
+      return initQuestions();
+    } else {
+      return createTeam();
+    }
+  }
 
-function init() {
-    return inquirer.prompt(managerQuestions);
-};
-
-init()
-    .then(data => {
-        return new Manager(data);
-    })
-    .then(data => {
-        const managerEntry = data;
-        console.log(data);
-        employeeList.push(managerEntry);
-        console.log(employeeList);
-    // })
-    // .then(addEmployee)
-    // .catch(err => {
-    //     console.log(err);
-    });
+  function createTeam() {
+    fs.writeFileSync("./dist/index.html", generateTeam(staffArray), "utf-8");
+  }
+  
+  initQuestions();
