566908e31dab67e769882feacd0f69ebd5859e7b
steven rosa <scrosa13@outlook.com>
2022-10-05 11:58:14 -0400

    updating tests

--- __tests__/intern.test.js
+++ __tests__/intern.test.js
@@ -1,8 +1,8 @@
-import Engineer from "../lib/engineer";
+import Intern from "../lib/intern";
 
 test('get name test', () => {
     const testing = 'john';
-    const testrun = new Engineer (testing, 1, 'smith@gmail.com', 'userName1')
+    const testrun = new Intern (testing, 1, 'smith@gmail.com', 'userName1')
 
     expect(testrun.name).toBe(testing)
 });
@@ -10,21 +10,21 @@ test('get name test', () => {
 
 test('get id test', () => {
     const testing = 1;
-    const testrun = new Engineer ('john', testing, 'smith@gmail.com','userName1')
+    const testrun = new Intern ('john', testing, 'smith@gmail.com','userName1')
 
     expect(testrun.id).toBe(testing)
 });
 
 test('get email test', () => {
     const testing = 'smith@gmail.com';
-    const testrun = new Engineer ('john', 1, testing, 'userName1')
+    const testrun = new Intern ('john', 1, testing, 'userName1')
 
     expect(testrun.email).toBe(testing)
 });
 
 test('get username test', () => {
     const testing = 'userName1';
-    const testrun = new Engineer ('john', 1, 'smith@gmail.com', testing)
+    const testrun = new Intern ('john', 1, 'smith@gmail.com', testing)
 
     expect(testrun.username).toBe(testing)
 });
